dcb4c61c360aa5939557421373e45d6a5a14e539
Elaine Pei Ling Chong <elaine.chong@catapultsports.com>
2021-01-26 12:05:06 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App: () => React$Node = () => {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+
+
+};
+
+export default App;
